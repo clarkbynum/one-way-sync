ec696de871a165e051ad7c68bdbf60d0e4f85df7
Clark Bynum <cbynum@clearblade.com>
2020-02-12 18:52:30 -0600

    init commit

---
+++ code/libraries/edgeDisconnected/edgeDisconnected.js
@@ -0,0 +1,15 @@
+/**
+ * Type: Module
+ * Description: A library that contains a function which, when called, returns an object with a public API.
+ */
+function edgeDisconnected(){
+    function function1() {}
+    function function2() {}
+    function function3() {}
+
+    return {
+        function1,
+        function2,
+        function3
+    }
+}
